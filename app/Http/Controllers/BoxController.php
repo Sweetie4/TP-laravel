f0c0fb0df063f4c9f96d245e276b3565c3f172fb
Mahora Grolleau <hiphopfraise@gmail.com>
2024-10-03 13:54:38 +0200

    #2 feat: migration/model/controller box

---
+++ app/Http/Controllers/BoxController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Box;
+use Illuminate\Http\Request;
+
+class BoxController extends Controller
+{
+    public function index(){
+        dd(Box::all());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\BoxController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/box', [BoxController::class, 'index'])->middleware(['auth', 'verified'])->name('box');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
