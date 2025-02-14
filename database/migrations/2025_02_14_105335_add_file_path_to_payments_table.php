b5d0482cc196d51e9f921dfa5d66bcdbcb1b6042
Mahora Grolleau <hiphopfraise@gmail.com>
2025-02-14 15:16:19 +0100

    #8 Fix(bills) : add default value to file_path

--- database/migrations/2025_02_14_105335_add_file_path_to_payments_table.php
+++ database/migrations/2025_02_14_105335_add_file_path_to_payments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('payments', function (Blueprint $table) {
-            $table->string('file_path');
+            $table->string('file_path')->default('');
         });
     }
 
